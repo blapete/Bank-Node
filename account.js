98eba8d9d4c5e707deef95b2ddd9c34a611538ad
Peebs <blank-p@outlook.com>
2022-12-01 08:45:51 -0600

    account

--- account.js
+++ account.js
@@ -7,13 +7,15 @@ class AbortTransaction extends Error {
 }
 
 class Account {
-  constructor(password) {
-    // this.name = name;
-    // this.balance = validateAmount(balance);
+  constructor(name, balance, password) {
+    this.name = name;
+    this.balance = this.validateAmount(balance);
     this.password = password;
   }
 
-  //   validateAmount(amount) {}
+  validateAmount(amount) {
+    console.log(amount);
+  }
 
   checkPasswordMatch(password) {
     if (password != this.password) {
@@ -21,17 +23,26 @@ class Account {
     }
   }
 
-  //   deposit() {}
+  deposit(amountToDeposit) {
+    amountToDeposit = this.validateAmount(amountToDeposit);
+    this.balance = this.balance + amountToDeposit;
+    return this.balance;
+  }
 
-  //   getBalance() {
-  //     return this.balance;
-  //   }
+  getBalance() {
+    return this.balance;
+  }
 
-  //   withdraw() {}
+  withdraw(amountToWithdraw) {
+    amountToWithdraw = this.validateAmount(amountToWithdraw);
+    if (amountToWithdraw > this.balance) throw new AbortTransaction('Insufficient funds');
+
+    this.balance = this.balance - amountToWithdraw;
+  }
 
   show() {
-    // console.log('       Name:', this.name);
-    // console.log('       Balance:', this.balance);
+    console.log('       Name:', this.name);
+    console.log('       Balance:', this.balance);
     console.log('       Password:', this.password);
   }
 }
@@ -40,6 +51,6 @@ const oAccount = new Account('test');
 
 // console.log(oAccount);
 
-console.log(oAccount.checkPasswordMatch('test1'));
+// console.log(oAccount.checkPasswordMatch('test1'));
 
 // console.log(oAccount.show());

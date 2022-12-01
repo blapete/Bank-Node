2661b18875ad018803274a7060d92baeb352f8b1
Peebs <blank-p@outlook.com>
2022-12-01 11:47:33 -0600

    bank

--- bank.js
+++ bank.js
@@ -1,5 +1,38 @@
 const { AbortTransaction, Account } = require('./account');
 
+class Bank {
+  constructor({ hours, address, phone }) {
+    this.accountsObj = {};
+    this.nextAccountNumber = 0;
+    this.hours = hours;
+    this.address = address;
+    this.phone = phone;
+  }
+
+  askForValidAccountNumber() {}
+
+  getUsersAccount() {}
+
+  askForValidPassword() {}
+
+  createAccount() {}
+
+  openAccount() {}
+
+  closeAccount() {}
+
+  balance() {}
+
+  deposit() {}
+
+  withdraw() {}
+
+  getInfo() {}
+
+  // for bank admin
+  show() {}
+}
+
 const oAccount = new Account({ name: 'peter', balance: 4.5, password: 'test' });
 
 console.log(oAccount);

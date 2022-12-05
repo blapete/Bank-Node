cf08bde03af301c6570b6f696ace3dc5d7cf371b
Peebs <blank-p@outlook.com>
2022-12-05 11:48:13 -0600

    prettier

--- account.js
+++ account.js
@@ -1,59 +1,60 @@
 /* custom exception */
 class AbortTransaction extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'Transaction aborted';
-  }
+   constructor(message) {
+      super(message);
+      this.name = 'Transaction aborted';
+   }
 }
 
 /* account class*/
 class Account {
-  constructor(userName, userPassword, userStartingAmount) {
-    if (!userName || !userPassword || !userStartingAmount)
-      throw new AbortTransaction('All fields required');
+   constructor(userName, userPassword, userStartingAmount) {
+      if (!userName || !userPassword || !userStartingAmount)
+         throw new AbortTransaction('All fields required');
 
-    this.name = userName;
-    this.password = userPassword;
-    this.balance = this.validateAmount(userStartingAmount);
-  }
+      this.name = userName;
+      this.password = userPassword;
+      this.balance = this.validateAmount(userStartingAmount);
+   }
 
-  validateAmount(amount) {
-    if (isNaN(parseFloat(amount)))
-      throw new AbortTransaction('Amount must be an integer');
-    if (amount < 0) throw new AbortTransaction('Amount must be positive');
+   validateAmount(amount) {
+      if (isNaN(parseFloat(amount)))
+         throw new AbortTransaction('Amount must be an integer');
+      if (amount < 0) throw new AbortTransaction('Amount must be positive');
 
-    return amount;
-  }
+      return amount;
+   }
 
-  checkPasswordMatch(password) {
-    if (password != this.password) throw new AbortTransaction('Incorrect Password');
-  }
+   checkPasswordMatch(password) {
+      if (password != this.password) throw new AbortTransaction('Incorrect Password');
+   }
 
-  deposit(amountToDeposit) {
-    amountToDeposit = this.validateAmount(amountToDeposit);
-    this.balance = this.balance + amountToDeposit;
-    return this.balance;
-  }
+   deposit(amountToDeposit) {
+      amountToDeposit = this.validateAmount(amountToDeposit);
+      this.balance = this.balance + amountToDeposit;
+      return this.balance;
+   }
 
-  withdraw(amountToWithdraw) {
-    amountToWithdraw = this.validateAmount(amountToWithdraw);
-    if (amountToWithdraw > this.balance) throw new AbortTransaction('Insufficient funds');
+   withdraw(amountToWithdraw) {
+      amountToWithdraw = this.validateAmount(amountToWithdraw);
+      if (amountToWithdraw > this.balance)
+         throw new AbortTransaction('Insufficient funds');
 
-    this.balance = this.balance - amountToWithdraw;
-  }
+      this.balance = this.balance - amountToWithdraw;
+   }
 
-  getBalance() {
-    return this.balance;
-  }
+   getBalance() {
+      return this.balance;
+   }
 
-  show() {
-    console.log('       Name:', this.name);
-    console.log('       Balance:', this.balance);
-    console.log('       Password:', this.password);
-  }
+   show() {
+      console.log('       Name:', this.name);
+      console.log('       Balance:', this.balance);
+      console.log('       Password:', this.password);
+   }
 }
 
 module.exports = {
-  AbortTransaction,
-  Account,
+   AbortTransaction,
+   Account,
 };
--- bank.js
+++ bank.js
@@ -2,110 +2,112 @@ const { AbortTransaction, Account } = require('./account');
 
 /* bank class - object manager object */
 class Bank {
-  #password;
+   #password;
 
-  constructor({ name, hours, address, phone, adminPassword }) {
-    this.accountsObj = {};
-    this.nextAccountNumber = 0;
-    this.name = name;
-    this.hours = hours;
-    this.address = address;
-    this.phone = phone;
-    this.#bankAdminPassword = adminPassword;
-  }
+   constructor({ name, hours, address, phone, adminPassword }) {
+      this.accountsObj = {};
+      this.nextAccountNumber = 0;
+      this.name = name;
+      this.hours = hours;
+      this.address = address;
+      this.phone = phone;
+      this.#bankAdminPassword = adminPassword;
+   }
 
-  get #bankAdminPassword() {
-    return this.#password;
-  }
+   get #bankAdminPassword() {
+      return this.#password;
+   }
 
-  set #bankAdminPassword(adminPassword) {
-    this.#password = adminPassword;
-  }
+   set #bankAdminPassword(adminPassword) {
+      this.#password = adminPassword;
+   }
 
-  async askForValidAccountNumber(promptUser) {
-    let accountNumber = await promptUser('What is your account number? ');
-    if (isNaN(parseInt(accountNumber)))
-      throw new AbortTransaction('Amount must be an integer');
-    if (!(accountNumber in this.accountsObj))
-      throw new AbortTransaction(`There is no account  ${accountNumber} `);
-    return accountNumber;
-  }
+   async askForValidAccountNumber(promptUser) {
+      let accountNumber = await promptUser('What is your account number? ');
+      if (isNaN(parseInt(accountNumber)))
+         throw new AbortTransaction('Amount must be an integer');
+      if (!(accountNumber in this.accountsObj))
+         throw new AbortTransaction(`There is no account  ${accountNumber} `);
+      return accountNumber;
+   }
 
-  async getUsersAccount(promptUser) {
-    let accountNumber = await this.askForValidAccountNumber(promptUser);
-    let theAccount = this.accountsObj[accountNumber];
-    await this.askForValidPassword(promptUser, theAccount);
-    return theAccount;
-  }
+   async getUsersAccount(promptUser) {
+      let accountNumber = await this.askForValidAccountNumber(promptUser);
+      let theAccount = this.accountsObj[accountNumber];
+      await this.askForValidPassword(promptUser, theAccount);
+      return theAccount;
+   }
 
-  async askForValidPassword(promptUser, theAccount) {
-    let password = await promptUser('Please enter your password');
-    theAccount.checkPasswordMatch(password);
-  }
+   async askForValidPassword(promptUser, theAccount) {
+      let password = await promptUser('Please enter your password');
+      theAccount.checkPasswordMatch(password);
+   }
 
-  createAccount({ name, password, amount }) {
-    let theAccount = new Account(name, password, amount);
-    let newAccountNumber = this.nextAccountNumber;
-    this.accountsObj[newAccountNumber] = theAccount;
-    this.nextAccountNumber = this.nextAccountNumber + 1;
-    return newAccountNumber;
-  }
+   createAccount({ name, password, amount }) {
+      let theAccount = new Account(name, password, amount);
+      let newAccountNumber = this.nextAccountNumber;
+      this.accountsObj[newAccountNumber] = theAccount;
+      this.nextAccountNumber = this.nextAccountNumber + 1;
+      return newAccountNumber;
+   }
 
-  async openAccount(promptUser) {
-    console.log('*** Open Account ***');
-    let userName = await promptUser('What is your username? ');
-    let userPassword = await promptUser('Create a new password ');
-    let userStartingAmount = await promptUser('How much is your initial deposit? ');
-    let userAccountNumber = this.createAccount({
-      name: userName,
-      password: userPassword,
-      amount: userStartingAmount,
-    });
-    console.log('Account created, account # ', userAccountNumber);
-  }
+   async openAccount(promptUser) {
+      console.log('*** Open Account ***');
+      let userName = await promptUser('What is your username? ');
+      let userPassword = await promptUser('Create a new password ');
+      let userStartingAmount = await promptUser('How much is your initial deposit? ');
+      let userAccountNumber = this.createAccount({
+         name: userName,
+         password: userPassword,
+         amount: userStartingAmount,
+      });
+      console.log('Account created, account # ', userAccountNumber);
+   }
 
-  closeAccount() {
-    console.log('*** Close Account ***');
-  }
+   closeAccount() {
+      console.log('*** Close Account ***');
+   }
 
-  async balance(promptUser) {
-    console.log('*** Get Balance ***');
-    let theAccount = await this.getUsersAccount(promptUser);
-    let theBalance = await theAccount.getBalance();
-    console.log(`Total balance: ${theBalance}`);
-  }
+   async balance(promptUser) {
+      console.log('*** Get Balance ***');
+      let theAccount = await this.getUsersAccount(promptUser);
+      let theBalance = await theAccount.getBalance();
+      console.log(`Total balance: ${theBalance}`);
+   }
 
-  deposit() {
-    console.log(' *** Deposit ***');
-  }
+   deposit() {
+      console.log(' *** Deposit ***');
+   }
 
-  withdraw() {
-    console.log(' *** Withdraw ***');
-  }
+   withdraw() {
+      console.log(' *** Withdraw ***');
+   }
 
-  getInfo() {
-    console.log(`hours: ${this.hours} \nAddress: ${this.address} \nPhone: ${this.phone}`);
-  }
+   getInfo() {
+      console.log(
+         `hours: ${this.hours} \nAddress: ${this.address} \nPhone: ${this.phone}`,
+      );
+   }
 
-  /* for bank admin */
-  async admin(promptUser) {
-    console.log('*** Admin Login ***');
-    let access = await promptUser('What is the admin password? ');
-    if (access != this.#bankAdminPassword)
-      throw new AbortTransaction('Authentication failed');
-    if (!Object.keys(this.accountsObj).length)
-      return console.log(`\nThere are no accounts yet at ${this.name}`);
-    // print each account as a string to the terminal
-    for (const acct of Object.keys(this.accountsObj)) {
-      let out = `Account # ${acct} `;
-      for (let i = 0; i < Object.keys(this.accountsObj[acct]).length; i++) {
-        out += ` ${Object.keys(this.accountsObj[acct])[i]}: ${
-          this.accountsObj[acct][Object.keys(this.accountsObj[acct])[i]]
-        }`;
+   /* for bank admin */
+   async admin(promptUser) {
+      console.log('*** Admin Login ***');
+      let access = await promptUser('What is the admin password? ');
+      if (access != this.#bankAdminPassword)
+         throw new AbortTransaction('Authentication failed');
+      if (!Object.keys(this.accountsObj).length)
+         return console.log(`\nThere are no accounts yet at ${this.name}`);
+      // print each account as a string to the terminal
+      for (const acct of Object.keys(this.accountsObj)) {
+         let out = `Account # ${acct} `;
+         for (let i = 0; i < Object.keys(this.accountsObj[acct]).length; i++) {
+            out += ` ${Object.keys(this.accountsObj[acct])[i]}: ${
+               this.accountsObj[acct][Object.keys(this.accountsObj[acct])[i]]
+            }`;
+         }
+         console.log(out);
       }
-      console.log(out);
-    }
-  }
+   }
 }
 
 module.exports = { Bank };
